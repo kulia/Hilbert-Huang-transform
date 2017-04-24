d4e7836af521df9307a2d0fecc2f17bbb22cd084
Geir Kulia <geir@kulia.no>
2017-04-24 13:16:02 +0200

    Uses matlab functions

--- lib/alreadyIntrinsicModeFunction.m
+++ lib/alreadyIntrinsicModeFunction.m
@@ -13,24 +13,24 @@ function alreadyIMF = alreadyIntrinsicModeFunction(signal)
 	yMin = -yMin;
 
 	alreadyIMF = 1;
-	% if ( sum(abs(yMax(yMax<0))) > 0 ) & ( sum(abs(yMin(yMin>0))) > 0 )
-	% 	alreadyIMF = 0;
-	% 	% disp('Signal is IMF.');
-	% else
-	% 	alreadyIMF = 1;
-	% end
-
-
-	if sum(length(xMin)+length(xMax)) < 1 | length(yMax(yMax < 0)) + length(yMin(yMin > 0)) > 0
+	if ( sum(abs(yMax(yMax<0))) > 0 ) & ( sum(abs(yMin(yMin>0))) > 0 )
 		alreadyIMF = 0;
-		% disp('Signal is not IMF. Run EMD. (1)');
+		% disp('Signal is IMF.');
 	else
-		for index = 1:(length(xMax)-1)
-			if(length(xMin(xMin > xMax(index) & xMin < xMax(index+1))) > 1)
-				alreadyIMF = 0;
-				% disp('Signal is not IMF. Run EMD. (2)');
-				break;
-			end
-			% disp(['Progress: ' num2str(100*index/length(xMax)-1) ' %.'])
-		end
+		alreadyIMF = 1;
 	end
+
+
+	% if sum(length(xMin)+length(xMax)) < 1 | length(yMax(yMax < 0)) + length(yMin(yMin > 0)) > 0
+	% 	alreadyIMF = 0;
+	% 	% disp('Signal is not IMF. Run EMD. (1)');
+	% else
+	% 	for index = 1:(length(xMax)-1)
+	% 		if(length(xMin(xMin > xMax(index) & xMin < xMax(index+1))) > 1)
+	% 			alreadyIMF = 0;
+	% 			% disp('Signal is not IMF. Run EMD. (2)');
+	% 			break;
+	% 		end
+	% 		% disp(['Progress: ' num2str(100*index/length(xMax)-1) ' %.'])
+	% 	end
+	% end
